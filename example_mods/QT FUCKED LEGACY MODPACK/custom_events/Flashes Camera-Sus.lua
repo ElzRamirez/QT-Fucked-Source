6c9356e4ba0092bca866ec28b5ebbc674f4f5aba
zRamírez <VirgateSnow3414@gmail.com>
2025-05-18 14:51:44 -0600

    fuckedmination... VIP!?

--- example_mods/QT FUCKED LEGACY MODPACK/custom_events/Flashes Camera-Sus.lua
+++ example_mods/QT FUCKED LEGACY MODPACK/custom_events/Flashes Camera-Sus.lua
@@ -3,14 +3,14 @@ function onEvent(n,v1,v2)
 		if getPropertyFromClass('ClientPrefs', 'flashing') == true then
 			makeLuaSprite('flash', '', 0, 0);
 			makeGraphic('flash',1280,720,'0065d8')
-			setObjectCamera('flash', 'camGameOverlay')
+			setObjectCamera('flash', 'camGame')
 			addLuaSprite('flash', true);
 			setLuaSpriteScrollFactor('flash',0,0)
 			setProperty('flash.scale.x',3)
 			setProperty('flash.scale.y',3)
 			setProperty('flash.alpha',0)
 			setProperty('flash.alpha',0.5)
-			doTweenAlpha('flTw','flash',0,v1,'linear')
+			doTweenAlpha('flTw','flash',0,0.5,'linear')
 		end
 	end
 end
